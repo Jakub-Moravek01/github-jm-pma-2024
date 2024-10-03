530f6d6c0813c0dbd497192a3f37db9495c242d5
Jakub Morávek <jakub.moravek@tul.cz>
2024-10-03 15:24:38 +0200

    #3 nastavení výpisu objednávky

--- MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
+++ MyApp003Objednavka/app/src/main/java/com/example/myapp003objednavka/MainActivity.kt
@@ -1,23 +1,48 @@
 package com.example.myapp003objednavka
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp003objednavka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
+        //binding settings
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        } */
+
         title = "Objednávka kosmetických předmětů"
+
+        //nastavení obsluhy tlačítka "objednat"
+
+        binding.btObjednavka.setOnClickListener {
+            //načtení ID vybraného radioButton z radioGroup
+            val cosmeticRbId = binding.rgCosmetic.checkedRadioButtonId
+
+            //díky tomuto řádku zobrazíme text a ne ID
+            val cosmetic = findViewById<RadioButton>(cosmeticRbId)
+
+            //zjistí, jestli je checkbox zaškrtnutný
+            val facNew = binding.cbFactoryNew.isChecked
+            val stattrak = binding.cbStattrak.isChecked
+            val batSca = binding.cbBattleScared.isChecked
+
+            //předávání hodnot do souhrnu objednávky
+            val objednavkaText = "Souhrn objednávky: " + "${cosmetic.text}" + (if(facNew) "; factory new" else "") +
+                    (if(stattrak) "; počítadlo pro kamarády" else "") + (if(batSca) "; battle scared" else "")
+
+            binding.tvShrnuti.text = objednavkaText
+        }
     }
 }
