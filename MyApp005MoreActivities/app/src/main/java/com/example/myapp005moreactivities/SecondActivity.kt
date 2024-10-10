464634bb7f907c0bc05fe6a94d4aa6b51d17d49e
Jakub Morávek <jakub.moravek@tul.cz>
2024-10-10 15:15:07 +0200

    #5 ukončení druhé aktivity

--- MyApp005MoreActivities/app/src/main/java/com/example/myapp005moreactivities/SecondActivity.kt
+++ MyApp005MoreActivities/app/src/main/java/com/example/myapp005moreactivities/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapp005moreactivities
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,18 @@ import androidx.core.view.WindowInsetsCompat
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvInfo = findViewById<TextView>(R.id.tvInfo)
+
+        //Načtení dat z intentu
+        val nickname = intent.getStringExtra("NICK_NAME")
+        tvInfo.text = "Data z první aktivity: $nickname"
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
         }
     }
 }
