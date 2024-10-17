e9fe367417cd17f7061da4c7bb93cc33d75f4300
Jakub Morávek <jakub.moravek@tul.cz>
2024-10-17 11:39:46 +0200

    #6 snackbar a jeho definice

--- MyApp006SnackBarToast/app/src/main/java/com/example/myapp006snackbartoast/MainActivity.kt
+++ MyApp006SnackBarToast/app/src/main/java/com/example/myapp006snackbartoast/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.myapp006snackbartoast
 
+import android.app.ActionBar
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapp006snackbartoast.databinding.ActivityMainBinding
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     //binding nastavení
@@ -27,6 +31,11 @@ class MainActivity : AppCompatActivity() {
         binding.btnShowToast.setOnClickListener {
             val toast = Toast.makeText(this, "Nazdar - mám hlad", Toast.LENGTH_LONG)
             toast.show()
+        }
+
+        // Nastavení akce pro tlačítko snackbar
+        binding.btnShowSnackBar.setOnClickListener {
+           Snackbar.make(binding.root, "Mám hlad", Snackbar.LENGTH_LONG).show()
         }
 
     }
