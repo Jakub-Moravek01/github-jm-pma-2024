d9a9bb013ab70b04a5fdd897329d975dedb4c319
Jakub Morávek <jakub.moravek@tul.cz>
2024-10-24 11:44:36 +0200

    #7  nastavení logiky pro main aktivitu

--- MyApp008aFragments/app/src/main/java/com/example/myapp008afragments/MainActivity.kt
+++ MyApp008aFragments/app/src/main/java/com/example/myapp008afragments/MainActivity.kt
@@ -5,12 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.myapp008afragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //definice bindingu
+    lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //obsluha prvního tlačítka
+        binding.btnFragment1.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+
+        //obsluha druhého tlačítka
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
     }
+private fun replaceFragment(fragment : Fragment) {
+    //získá instanci správce fragmentu
+    val fragmentManager = supportFragmentManager
+
+    //vytvoří novou transakci s fragmenty
+    val fragmentTransaction = fragmentManager.beginTransaction()
+
+    //nahradí fragment v kontejneru novým fragmentem, který byl předán jako tzv. argument
+    fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+
+    //potvrdí transakci a provede výměnu fragmentu
+    fragmentTransaction.commit()
+}
 }
