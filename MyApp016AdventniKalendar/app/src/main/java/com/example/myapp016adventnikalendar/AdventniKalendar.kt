39bbebfc598d6dae440ad684a64d383b286733e4
Jakub Morávek <jakub.moravek@tul.cz>
2024-12-12 12:03:51 +0100

    #13 přidání logiky tlačítka pro adventní kalendář, vytvoření layoutu pro kalendář a návrat zpět do původní aktivity

---
+++ MyApp016AdventniKalendar/app/src/main/java/com/example/myapp016adventnikalendar/AdventniKalendar.kt
@@ -0,0 +1,20 @@
+package com.example.myapp016adventnikalendar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AdventniKalendar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_adventni_kalendar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
