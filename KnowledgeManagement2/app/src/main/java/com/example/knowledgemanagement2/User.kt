8ff148dce3cd62aeab525d2b3faf2ca65bec47eb
Jakub-Moravek01 <jakub.moravek@tul.cz>
2025-01-31 19:47:05 +0100

    Zprovoznění gradle a příprava User.kt

---
+++ KnowledgeManagement2/app/src/main/java/com/example/knowledgemanagement2/User.kt
@@ -0,0 +1,15 @@
+package com.example.knowledgemanagement
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val firstName: String,
+    val lastName: String,
+    val username: String,
+    val email: String,
+    val password: String,
+    val area: String
+)
