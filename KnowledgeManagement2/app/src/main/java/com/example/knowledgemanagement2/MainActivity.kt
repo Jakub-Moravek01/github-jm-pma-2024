17b68f7fce6533ffd874f3fb0d328e770800e23a
Jakub-Moravek01 <jakub.moravek@tul.cz>
2025-01-31 20:46:48 +0100

    Funkcionalita přihlášení

--- KnowledgeManagement2/app/src/main/java/com/example/knowledgemanagement2/MainActivity.kt
+++ KnowledgeManagement2/app/src/main/java/com/example/knowledgemanagement2/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.knowledgemanagement2
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
+import androidx.lifecycle.lifecycleScope
 import com.example.knowledgemanagement2.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,9 +24,33 @@ class MainActivity : AppCompatActivity() {
         database = AppDatabase.getDatabase(this)
         userDao = database.userDao()
 
+        // Odkaz na registraci
         binding.btnRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+
+        // Přihlášení
+        binding.btnLogin.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            // Zkontrolujeme, jestli uživatel existuje v databázi
+            lifecycleScope.launch {
+                val user = userDao.getUserByUsername(username)
+
+                if (user != null && user.password == password) {
+                    // Pokud uživatel existuje a heslo je správné, zobrazíme toast
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Přihlášení úspěšné", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Pokud nejsou shody, ukážeme chybovou hlášku
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Chybné přihlašovací údaje", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
